eee68b15b27bb44f041aad9126bcf657eb1ae710
Chrystian Lozano <chrystianlozano@icloud.com>
2020-02-25 09:13:13 -0600

    componente Formulario

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import './App.css';
+import React, {Fragment} from 'react';
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
 
 function App() {
   return (
-    <h1>Clima</h1>
+    <Fragment>
+      <Header titulo="Clima React App" />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario />
+            </div>
+            <div className="col m6 s12">2</div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
